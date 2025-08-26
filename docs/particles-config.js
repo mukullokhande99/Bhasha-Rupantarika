c19f404336977c7adc71b2394b6cee5516a6b1c2
Mukul <36757243+mukullokhande99@users.noreply.github.com>
2025-08-27 01:55:29 +0530

    Create particles-config.js

---
+++ docs/particles-config.js
@@ -0,0 +1,58 @@
+particlesJS('particles-js', {
+    "particles": {
+        "number": {
+            "value": 80,
+            "density": {
+                "enable": true,
+                "value_area": 800
+            }
+        },
+        "color": {
+            "value": "#ffd700"  // gold colored particles
+        },
+        "shape": {
+            "type": "circle"
+        },
+        "opacity": {
+            "value": 0.6,
+            "random": false
+        },
+        "size": {
+            "value": 3,
+            "random": true
+        },
+        "line_linked": {
+            "enable": true,
+            "distance": 150,
+            "color": "#ffd700",
+            "opacity": 0.4,
+            "width": 1
+        },
+        "move": {
+            "enable": true,
+            "speed": 2,
+            "direction": "none",
+            "random": true,
+            "straight": false,
+            "out_mode": "out"
+        }
+    },
+    "interactivity": {
+        "detect_on": "canvas",
+        "events": {
+            "onhover": {
+                "enable": true,
+                "mode": "grab"
+            }
+        },
+        "modes": {
+            "grab": {
+                "distance": 140,
+                "line_linked": {
+                    "opacity": 0.8
+                }
+            }
+        }
+    },
+    "retina_detect": true
+});
